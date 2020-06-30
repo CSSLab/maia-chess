689d1aa3bcadb238c6f766d2096721e879f6b39c
Reid McIlroy-Young <reidmcy@gmail.com>
2020-06-30 03:35:45 -0400

    fixed up script

--- move_prediction/pgn_to_trainingdata.sh
+++ move_prediction/pgn_to_trainingdata.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
-testing_blocks=4
+max_procs=10
 
+#sorry these are relative only
+#remove the $PWD to make work with absolute paths
 input_file=$PWD/$1
 output_files=$PWD/$2
 mkdir -p $output_files
@@ -12,18 +15,41 @@ mkdir -p $output_files/validation
 
 cd $output_files/blocks
 
+#using tool from:
+#https://www.cs.kent.ac.uk/people/staff/djb/pgn-extract/
+
 pgn-extract -7 -C -N  -#1000 $input_file
 
 #use the first 3000 as validation set
-cat {1..3}.pgn > $output_files/validation/validation.pgn
-rm {1..3}.pgn
+mv {1..3}.pgn $output_files/validation/
 
-cat *.pgn > $output_files/training/training.pgn
+mv *.pgn $output_files/training/
 
-cd $output_files/training
+cd ..
 rm -rv $output_files/blocks
 
-trainingdata-tool -v ../../pgns/${s}_${c}.pgn
+for data_type in "training" "validation"; do
+    cd $output_files/$data_type
+    for p in *.pgn; do
+        cd $output_files/$data_type
+        p_num=${p%".pgn"}
+        echo "Starting on" $data_type $p_num
+        mkdir $p_num
+        cd $p_num
+        #using tool from:
+        #https://github.com/DanielUranga/trainingdata-tool
+        trainingdata-tool ../$p &
+        while [ `echo $(pgrep -c -P$$)` -gt $max_procs ]; do
+            printf "waiting\r"
+            sleep 1
+        done
+    done
+done
+echo "Almost done"
+wait
+echo "Done"
+
+
 
 
 
@@ -47,15 +73,13 @@ for player_file in $input_files/*.bz2; do
             mkdir -p $s
             mkdir $s/$c
 
-            #using tool from:
-            #https://www.cs.kent.ac.uk/people/staff/djb/pgn-extract/
+
             bzcat $split_dir/${s}_${c}.pgn.bz2 |
 
             cat *.pgn > pgns/${s}_${c}.pgn
             rm -v *.pgn
 
-             #using tool from:
-            #https://github.com/DanielUranga/trainingdata-tool
+
             screen -S "${p_name}-${c}-${s}" -dm bash -c "cd ${s}/${c}; trainingdata-tool -v ../../pgns/${s}_${c}.pgn"
         done
         cd -
